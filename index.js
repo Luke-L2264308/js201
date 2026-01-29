e18ab3fa3d15e9b240d0ed26e57960f1e72d190a
Luke Linington <lukelinington@gmail.com>
2026-01-29 18:29:24 +0000

    Add array utility functions: arraySet, addAll, larger, largest, and compare

--- index.js
+++ index.js
@@ -43,3 +43,34 @@ function checkObjectInside(obj){
     obj.data.checked = true;
 }
 
+function arraySet(arr,i,n){
+  if (arr[i] != null){
+    arr[i] = n
+  }
+}
+
+function addAll(arr){
+  let result = 0
+  for (let i = 0; i<arr.length; i++){
+    result += arr[i]
+  }
+  return result
+}
+
+function larger(a,b){
+  return (a>b) ? a: b
+}
+
+function largest(arr){
+  let largest = (arr.length > 0)? arr[0]: null
+  for (let i = 0; i<arr.length; i++){
+    (arr[i]>largest) ? largest = arr[i] : null; 
+  }
+  return largest
+
+}
+
+function compare(a,b){ //comparing 2 arrays compares the array references rather than their contents
+  return (JSON.stringify(a) == JSON.stringify(b))? true:false 
+}
+
